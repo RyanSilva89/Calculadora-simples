4daf1a8295905c5c080e8ad07a263db45298a452
Ryan <r.sfariaa89@gmail.com>
2025-03-23 22:15:35 +0900

    testando interação no butão 7,no entanto apresenta um erro

---
+++ back-end/app.js
@@ -0,0 +1,9 @@
+let buttons= document.getElementById('buttons');
+let number=document.getElementById('adicionarNumero(7)');
+console.log(number);
+
+number.addEventListener("click",function(){
+
+    console.log("teste");
+});
+
