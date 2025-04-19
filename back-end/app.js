f6d9b6ba97ba8acc87edd82acc397672fe871167
Ryan <r.sfariaa89@gmail.com>
2025-04-19 21:14:45 +0900

    feat: implementa lógica da calculadora com tratamento de divisão por zero.

--- back-end/app.js
+++ back-end/app.js
@@ -1,14 +1,57 @@
-console.log("app.js carregado!");
-console.log("JavaScript funcionando!");
+                //verificando o carregamento do arquivo JS
+                console.log("app.js carregado!");
+                console.log("JavaScript funcionando!");
+            const botoes = document.querySelectorAll('#buttons button');
+            //Adicionando um valor na tela 
+            function adicionarNumero(valor){
+                    console.log(valor);
+                    const tela = document.getElementById('tela');
+                    if(tela.innerText === '0'){
+                        tela.innerText = valor;
 
-function adicionarNumero(valor){
-    console.log(valor);
-    const tela = document.getElementById('tela');
-    if(tela.innerText === '0'){
-        tela.innerText = valor;
+                    }else{ 
+                        tela.innerText += valor;
+                    }
+                        
+                }
+                    function  AdicionarOperacao(operacao){
+                    tela.innerText += `${operacao}`;
+                    }
+                    //limpar tela
+                    function limparTela(limparTela){
+                        tela.innerText = '0';
 
-    }else{ 
-        tela.innerText += valor;
-    }
-        
-}
+                            
+                        }
+            function calcular(){
+                const tela = document.getElementById('tela');
+                
+                try { 
+                    const expressao = tela.innerText;
+                if(expressao.includes ( '0')){
+                    tela.innerText = "Erro divisão por zero";
+
+                }else{
+                    tela.innerText = eval(expressao);
+
+                }
+                
+                } catch (error) {
+                    tela.innerText = 'Erro';
+                }
+            
+        }
+                        botoes.forEach(botao => {
+                            botao.addEventListener('click',function(){
+                                if(botao.classList.contains('numero')){
+            adicionarNumero(botao.innerText);
+                                }else if(botao.classList.contains('operacao')){
+                                    adicionarNumero(botao.innerText);
+                                }else if (botao.id === 'limparTela'){
+                                    limparTela();
+
+                                }else if(botao.id === 'calcular'){
+                                    calcular();
+                                }
+                            })
+                        });
