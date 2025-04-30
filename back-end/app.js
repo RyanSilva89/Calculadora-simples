9605a3be022567fe6e557f228ffc44d2c1c5bd82
Ryan Silva. <r.sfariaa89@gmail.com.>
2025-04-30 09:47:19 +0900

    Refactor: Melhorias na organização e segurança do código
    - Centralizada a verificação de erros nas funções específicas.
    - Substituído o uso de `eval()` por `new Function()` para maior segurança ao calcular expressões.
    - Evitada repetição de código ao melhorar as condições de verificação da tela.
    - Melhoria na estrutura e legibilidade do código.
    - Adição de verificação para impedir operadores consecutivos e divisão por zero.

--- back-end/app.js
+++ back-end/app.js
@@ -3,7 +3,7 @@
                 console.log("JavaScript funcionando!");
             const botoes = document.querySelectorAll('#buttons button');
             
-            //Adicionando um valor na tela 
+            //função Adicionando um valor na tela 
             function AdicionarNumero(valor){
                     console.log(valor);
                     const tela = document.getElementById('tela');
@@ -18,62 +18,58 @@
                         
                 }
 
-                //operadores
+                //função de operadores
 function  AdicionarOperacao(operacao){ // função 
 const tela = document.getElementById('tela');
 const textoAtual = tela.innerText;
 const ultimaPosicao = tela.innerText[tela.innerText.length - 1];
 const operadores = ['+','-','*','/']; // array com operadores válidos
 
-//Impedindo a digitação ao ocorrer um erro 'operadores'
+//Impedindo a digitação ao ocorrer um erro 
 if(tela.innerText.includes("Erro")){
-    tela.innerText = operacao ;
+tela.innerText = operacao ;
 return;
 }
 //Impedir que o usuario digite um operador antes de um número
 if(textoAtual === '0'&&(operacao === '*'|| operacao === '/' || operacao === '+')){
 return;
 }        
-//Nao adiciona um operador se o ultimo for um operador : 5++
+//Impedindo dois operadores conecutivos
 if (operadores.includes(ultimaPosicao)){
 return;
 }
-
-if ( textoAtual === '0'){
-tela.innerText = operacao;
-
-}else{
-tela.innerText += operacao;
-}
+tela.innerText=textoAtual === '0' ? operacao : textoAtual + operacao;
 
 
 
 }
-    //limpar tela
+    //função limpar tela
 function limparTela(){
     const tela = document.getElementById('tela');
                         tela.innerText = '0';
 
                             
                         }
-                        //mostrado o resultado da operação,possui restrição por divisão de zero
+                        //função calcular o resultado
             function calcular(){
                 const tela = document.getElementById('tela');
                 const operadores = ['+','-','*','/'];
                 const expressao = tela.innerText;
+
                 //regex para divisão por zero
                     const divisaoPorZero = /\/0+(?![\d.])/; //irá detectar /0 / 0.0 //000 
                if(divisaoPorZero.test(expressao)){
                 tela.innerText = "Erro: divisão por zero";
                }else{
                 try{
-                    tela.innerText = eval(expressao);
+                    tela.innerText = new Function('return' + expressao) ();
+                    
                 }catch(error){
                     tela.innerText = 'Erro';
                 }
                }
             
-        }
+        }                  //Adiocionando evento ao botão usando listeners
                         botoes.forEach(botao => {
                             botao.addEventListener('click',function(){
                                 if(botao.classList.contains('numero')){
